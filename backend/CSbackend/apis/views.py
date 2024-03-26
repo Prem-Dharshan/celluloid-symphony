976c50cc6790a612f962a9e31ff0b312e424fa6b
PREM DHARSHAN D <84702489+Prem-Dharshan@users.noreply.github.com>
2024-03-26 13:38:15 +0530

    Add API key from TMDB

--- backend/CSbackend/apis/views.py
+++ backend/CSbackend/apis/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+import requests
+from django.http import JsonResponse
 
 # Create your views here.
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class MovieDetailView():
+
+    def post(request):
+
+        api_key = os.environ.get('APIKEY')
+        
+
+    

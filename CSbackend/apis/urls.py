b07e6b77ad949fe0e124ed4181f011d8b0e4868c
PREM DHARSHAN D <84702489+Prem-Dharshan@users.noreply.github.com>
2024-03-27 20:50:58 +0530

    Add TVshow search view

--- CSbackend/apis/urls.py
+++ CSbackend/apis/urls.py
@@ -3,8 +3,11 @@ from .views import *
 
 urlpatterns = [
 
-    path('movie-search/<str:movie_name>',
-         MovieSearchView.as_view(), name='movie-search'),
+    path('search-movie/<str:movie_name>',
+         MovieSearchView.as_view(), name='search-movie'),
+    path('search-tv-show/<str:tv_show_name>',
+         TVShowSearchView.as_view(), name='search-tv-show'),
+
     path('movie-detail/<int:movie_id>',
          MovieDetailView.as_view(), name='movie-detail'),
 ]
--- CSbackend/apis/views.py
+++ CSbackend/apis/views.py
@@ -3,7 +3,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .views import *
-import tmdbsimple as tmdb
 import os
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
@@ -65,13 +64,16 @@ class MovieSearchView(APIView):
     )
     def get(self, request, movie_name):
 
-        url = f"https://api.themoviedb.org/3/search/movie?query={movie_name}"
-        
+        url = f"https://api.themoviedb.org/3/search/movie"
+
         params = {
             'query': movie_name,
             'include_adult': request.query_params.get('include_adult', 'true'),
             'language': request.query_params.get('language', 'en-US'),
-            'page': request.query_params.get('page', 1)
+            'page': request.query_params.get('page', 1),
+            'primary_release_year': request.query_params.get('primary_release_year', ''),
+            'year': request.query_params.get('year', ''),
+            'region': request.query_params.get('region', ''),
         }
 
         headers = {
@@ -79,7 +81,8 @@ class MovieSearchView(APIView):
             "Authorization": f"Bearer {os.environ.get('ACCESSTOKENAUTH')}"
         }
 
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, params=params, headers=headers)
+        # print(response.url)
 
         if response.status_code == 200:
             return Response(response.json())
@@ -87,6 +90,79 @@ class MovieSearchView(APIView):
             return Response({"error": "Unauthorized request. Please check your bearer token."}, status=status.HTTP_401_UNAUTHORIZED)
         else:
             return Response({"error": "Failed to retrieve movie data"}, status=response.status_code)
+
+
+class TVShowSearchView(APIView):
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                name='first_air_date_year',
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_INTEGER,
+                required=False,
+                description='Search only the first air date. Valid values are: 1000..9999',
+            ),
+            openapi.Parameter(
+                name='include_adult',
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_BOOLEAN,
+                required=False,
+                description='Include adult content (true or false)',
+                default=False
+            ),
+            openapi.Parameter(
+                name='language',
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                required=False,
+                description='Language code (e.g., "en-US")',
+                default='en-US'
+            ),
+            openapi.Parameter(
+                name='page',
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_INTEGER,
+                required=False,
+                description='Page number',
+                default=1
+            ),
+            openapi.Parameter(
+                name='year',
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                required=False,
+                description='Release year of the movie',
+            ),
+        ]
+    )
+    def get(self, request, tv_show_name):
+
+        url = f"https://api.themoviedb.org/3/search/tv"
+
+        params = {
+            'query': tv_show_name,
+            'first_air_date_year': request.query_params.get('first_air_date_year', ''),
+            'include_adult': request.query_params.get('include_adult', 'true'),
+            'language': request.query_params.get('language', 'en-US'),
+            'page': request.query_params.get('page', 1),
+            'year': request.query_params.get('year', ''),
+        }
+
+        headers = {
+            "accept": "application/json",
+            "Authorization": f"Bearer {os.environ.get('ACCESSTOKENAUTH')}"
+        }
+
+        response = requests.get(url, params=params, headers=headers)
+        # print(response.url)
+
+        if response.status_code == 200:
+            return Response(response.json())
+        elif response.status_code == 401:
+            return Response({"error": "Unauthorized request. Please check your bearer token."}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({"error": "Failed to retrieve TV show data"}, status=response.status_code)
 
 
 class MovieDetailView(APIView):

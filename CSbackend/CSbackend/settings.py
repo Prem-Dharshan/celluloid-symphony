7b32204d9458fc57fc676f6c36a3b46ff65c770b
PREM DHARSHAN D <84702489+Prem-Dharshan@users.noreply.github.com>
2024-03-30 13:42:30 +0530

    cors orgin all set to true

--- CSbackend/CSbackend/settings.py
+++ CSbackend/CSbackend/settings.py
@@ -30,15 +30,15 @@ ALLOWED_HOSTS = environ.get('ALLOWED_HOSTS').split(',')
 # ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_WHITELIST = [
-    # 'http://localhost:3000',
-]
+# CORS_ORIGIN_WHITELIST = [
+#     # 'http://localhost:3000',
+# ]
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+# ]
 
-CSRF_COOKIE_NAME="csrftoken"
+# CSRF_COOKIE_NAME="csrftoken"
 
 # CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'DELETE']
 # CORS_ALLOW_HEADERS = ['Content-Type', 'Authorization']
